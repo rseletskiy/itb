b84922ffef7df926f42ac3109c35d4fe3f0cdc11
Roman Seletskiy <seletskiyweb@gmail.com>
2022-07-20 12:46:07 +0300

    Try to fix js

--- scripts/index.js
+++ scripts/index.js
@@ -236,6 +236,45 @@ try {
   console.error(e);
 }
 
+// mobile filters
+try {
+  const filterButtons = document.querySelectorAll('.js-m-filter-btn');
+  const filterWrappers = document.querySelectorAll('.js-m-filter-wrapper');
+  filterButtons.forEach(button => {
+    button.addEventListener('click', function(e) {
+      e.preventDefault();
+      const button = e.target;
+      let id = button.getAttribute('data-id');
+      let wrapper = document.getElementById(id);
+      resetFilter(id)
+      if(!button.classList.contains('active') &&
+          !wrapper.classList.contains('active')
+        ) {
+          button.classList.add('active')
+          wrapper.classList.add('active')
+        } else {
+          button.classList.remove('active')
+          wrapper.classList.remove('active')
+        }
+    })
+  })
+
+  function resetFilter(id) {
+    filterButtons.forEach(button => {
+      if (button.getAttribute('data-id') != id) {
+        button.classList.remove('active');
+      }
+    })
+
+    filterWrappers.forEach(wrapper => {
+      if(wrapper.getAttribute('id') != id) {
+        wrapper.classList.remove('active');
+      }
+    })
+  }
+} catch (e) {
+  console.error(e);
+}
 
 try {
   (function () {
@@ -309,46 +348,6 @@ try {
     breakpoint.addListener(breakpointChecker);
     breakpointChecker();
   })();
-} catch (e) {
-  console.error(e);
-}
-
-// mobile filters
-try {
-  const filterButtons = document.querySelectorAll('.js-m-filter-btn');
-  const filterWrappers = document.querySelectorAll('.js-m-filter-wrapper');
-  filterButtons.forEach(button => {
-    button.addEventListener('click', function(e) {
-      e.preventDefault();
-      const button = e.target;
-      let id = button.getAttribute('data-id');
-      let wrapper = document.getElementById(id);
-      resetFilter(id)
-      if(!button.classList.contains('active') &&
-          !wrapper.classList.contains('active')
-        ) {
-          button.classList.add('active')
-          wrapper.classList.add('active')
-        } else {
-          button.classList.remove('active')
-          wrapper.classList.remove('active')
-        }
-    })
-  })
-
-  function resetFilter(id) {
-    filterButtons.forEach(button => {
-      if (button.getAttribute('data-id') != id) {
-        button.classList.remove('active');
-      }
-    })
-
-    filterWrappers.forEach(wrapper => {
-      if(wrapper.getAttribute('id') != id) {
-        wrapper.classList.remove('active');
-      }
-    })
-  }
 } catch (e) {
   console.error(e);
 }
